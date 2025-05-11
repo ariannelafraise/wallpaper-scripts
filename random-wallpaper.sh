f97b26433fa4a640d311d66a25489eaadc4ba88c
ariannelafraise <github@shield.paintilya.dev>
2025-05-11 11:49:45 -0400

    prevent same wallpaper

--- random-wallpaper.sh
+++ random-wallpaper.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+script_dir=~/personal_dev/wallpaper-scripts
 randomWallpaper=$(ls ~/Wallpapers/active_wallpapers/*.png ~/Wallpapers/active_wallpapers/*.jpg | sort -R | tail -1)
+
+while [ "$randomWallpaper" = $(cat $script_dir/lastwallpaper) ]
+do
+    randomWallpaper=$(ls ~/Wallpapers/active_wallpapers/*.png ~/Wallpapers/active_wallpapers/*.jpg | sort -R | tail -1)
+done
+
+echo -n "$randomWallpaper" > $script_dir/lastwallpaper
 
 if [ "$1" = "startup" ]
 then
